b1472e14a10d84be3db36f20398600af45726c8d
jvelezpo <sebasvelez@gmail.com>
2019-09-14 15:02:07 -0500

    stubs and mocks

--- cypress/integration/2_posts.spec.js
+++ cypress/integration/2_posts.spec.js
@@ -2,6 +2,14 @@ describe('Test like there is no tomorrow', () => {
 
   it('Input should have same value as the one typed', () => {
     
+    cy.server()
+    cy.route('GET', '/api/posts*', [
+      {
+        "post": "medJS",
+        "likes": 0,
+        "id": 1 }
+    ])
+
     cy.visit('/')
     cy.get('.posts-list .post')
       .should('have.length', 1)
