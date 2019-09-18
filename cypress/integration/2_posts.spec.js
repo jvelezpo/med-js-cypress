b8da5a5bac1f085be98fc679d3603c12013a3797
jvelezpo <sebasvelez@gmail.com>
2019-09-18 09:57:28 -0500

    add asserts on stats

--- cypress/integration/2_posts.spec.js
+++ cypress/integration/2_posts.spec.js
@@ -6,12 +6,18 @@ describe('Test like there is no tomorrow', () => {
     cy.route('GET', '/api/posts*', [
       {
         "post": "medJS",
-        "likes": 0,
+        "likes": 3,
         "id": 1 }
     ])
 
     cy.visit('/')
     cy.get('.posts-list .post')
       .should('have.length', 1)
+
+    cy.get('.total-posts')
+      .should('have.text', '1')
+
+    cy.get('.total-posts-likes')
+      .should('have.text', '3')
   })
 })
